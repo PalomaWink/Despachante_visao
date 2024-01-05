5c3e7a1ba049d6ab9c170db4720990361943db7b
Paloma Wink <paloma.wink@hotmail.com>
2024-01-05 15:42:25 -0300

    feat: Criacao e estilizacao do footer e do navbar

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,21 @@
+'use client'
+//Elementos HTML
+import Link from "next/link"
+
+//Redes Sociais
+import Whatsapp from "./botoes/Whatsapp"
+import Instagram from "./botoes/Instagram"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <nav className="fixed bottom-0 w-full flex justify-center items-center bg-cinza-claro p-4 shadow-xl top-shadow"> 
+      <Link href="/" className="text-2xl sm:text-xl mx-2">
+        <Instagram />
+      </Link>
+      <Link href='/' className="text-2xl sm:text-xl mx-2">
+        <Whatsapp />
+      </Link>
+    </nav>
   )
 }
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,37 @@
+'use client'
 
+import Link from "next/link"
+import { useState } from "react"
 function Navbar() {
+  const [menu, setMenu] = useState(false)
+
+  function toggleMenu() {
+    setMenu(!menu)
+  }
+  
   return (
-    <div>Navbar</div>
+      <>
+      {menu && <div className="overlay" onClick={toggleMenu}></div>}
+      <nav className="flex justify-start shadow-lg bg-cinza-claro">
+        {/* Ícone do menu para telas pequenas */}
+        <div className="sm:hidden">
+          <button onClick={toggleMenu}>
+            <i className="bi bi-list"></i>
+          </button>
+        </div>
+
+        {/* Links do menu */}
+        <div className={`${menu ? 'block' : 'hidden'} sm:flex flex-grow justify-evenly items-center h-20`}>
+          <button onClick={toggleMenu} className="absolute top-0 right-0 p-4 sm:hidden">
+            <i className="bi bi-x-lg"></i>
+          </button>
+          <Link href="/" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Home</Link>
+          <Link href="/servicos" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Serviços</Link>
+          <Link href="/sobre" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Sobre nós</Link>
+          <Link href="/contato" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Contato</Link>
+        </div>
+      </nav>
+    </>
   )
 }
 
