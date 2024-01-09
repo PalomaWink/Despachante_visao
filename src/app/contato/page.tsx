cc9b6e1ebf0f0c8efca775e16bef6cd3c62a8539
Paloma Wink <paloma.wink@hotmail.com>
2024-01-09 14:24:14 -0300

    style: Estilizacao da pagina de contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import emailjs from '@emailjs/browser'
+import { BsEnvelopeAt, BsTelephone } from "react-icons/bs";
+import { FcPhoneAndroid } from "react-icons/fc";
 
 // Tipagem
 import { Contato } from "../type"
@@ -35,72 +37,109 @@ export default function Page() {
   })
 
   function onSubmitEmail(data: Contato) {
-    const templateParams = {
+    /* const templateParams = {
       from_name: data.nome,
       from_email: data.email,
       message: data.mensagem,
     }
-    emailjs.send("service_hj1qzof", "template_pxo1wuc", templateParams, "AqLuZzCqpb48hDAPj")
+    emailjs.send("service_um90tbr", "template_pxo1wuc", templateParams, "AqLuZzCqpb48hDAPj")
     .then((response) => {
       console.log('SUCCESS!', response.status, response.text);
       reset()
     }, (err) => {
       console.log('FAILED...', err);
-    })
+    }) */
   }
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <form
-        onSubmit={handleSubmit(onSubmitEmail)}
-        className="w-full max-w-xl border-laranja-forte border-2 border-solid rounded-md p-2 flex flex-col gap-4"
-      >
-        <div className="flex flex-col">
-          <label htmlFor="nome">Nome</label>
-          <input 
-            type="text"
-            id="nome"
-            className="border border-solid border-zinc-200 rounded shadow-sm h-10 px-3"
-            {...register("nome")} 
-          />
-          {errors.nome && <span className="text-vermelho text-sm">{errors.nome.message}</span>}
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="email">E-mail</label>
-          <input 
-            type="email" 
-            id="email"
-            className="border border-solid border-zinc-200 rounded shadow-sm h-10 px-3"
-            {...register("email")} 
-          />
-          {errors.email && <span className="text-vermelho text-sm">{errors.email.message}</span>}
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="mensagem">Mensagem</label>
-          <textarea 
-            id="mensagem"
-            className="border border-solid border-zinc-200 rounded shadow-sm h-20 px-3 py-2"
-            {...register("mensagem")} 
-          />
-          {errors.mensagem && <span className="text-vermelho text-sm">{errors.mensagem.message}</span>}
-        </div>
-        <div className="flex justify-center items-center mt-10 mb-2">
-          <button 
-            type="submit" 
-            className="bg-verde rounded-xl w-44 h-10 hover:bg-verde-escuro hover:text-white shadow-lg"
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl mb-10 text-laranja-claro font-bold text-center">Entre em contato conosco!</h1>
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-4 w-full">
+          <div className="col-span-2 flex flex-col justify-center items-end mr-9">
+            <span className="flex justify-center items-center w-96 mt-5">
+              <div className="mr-2">
+                <BsTelephone/>
+              </div>
+              (51) 3056-4808 <b className="ml-2">Escriório</b>
+            </span>
+            <span className="flex justify-center items-center w-96 mt-5">
+              <div className="mr-2">
+                <FcPhoneAndroid />
+              </div>
+              (51) 9 9673-9085 <b className="ml-2">Escriório</b>
+            </span>
+            <span className="flex justify-center items-center w-96 mt-5">
+              <div className="mr-2">
+                <FcPhoneAndroid />
+              </div>
+              (51) 9 9676-0159 <b className="ml-2">Guilherme</b>
+            </span>
+            <span className="flex justify-center items-center w-96 mt-5">
+              <div className="mr-2">
+                <FcPhoneAndroid />
+              </div>
+              (51) 9 9673-9209 <b className="ml-2">Francisco</b>
+            </span>
+            <span className="flex justify-center items-center w-96 mt-5 mb-5">
+              <div className="mr-2">
+                <BsEnvelopeAt />
+              </div>
+              despachantechico@hotmail.com
+            </span>
+            <nav className="flex flex-col justify-center items-center gap-4  w-96"> 
+              <Link href="/" className="text-2xl sm:text-xl mx-2">
+                <Instagram />
+              </Link>
+              <a href="https://wa.me/5551996739085" className="text-2xl sm:text-xl mx-2">
+                <Whatsapp />
+              </a>
+            </nav>
+          </div>
+          <form
+            onSubmit={handleSubmit(onSubmitEmail)}
+            className="col-span-2 w-full max-w-xl border-laranja-forte border-2 border-solid rounded-md p-2 flex flex-col gap-4"
           >
-            Enviar
-          </button>
+            <div className="flex flex-col">
+              <label htmlFor="nome">Nome</label>
+              <input 
+                type="text"
+                id="nome"
+                className="border border-solid border-zinc-200 rounded shadow-sm h-10 px-3 focus:border-laranja-forte"
+                {...register("nome")} 
+              />
+              {errors.nome && <span className="text-red-600 text-sm">{errors.nome.message}</span>}
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="email">E-mail</label>
+              <input 
+                type="email" 
+                id="email"
+                className="border border-solid border-zinc-200 rounded shadow-sm h-10 px-3 focus:border-laranja-forte"
+                {...register("email")} 
+              />
+              {errors.email && <span className="text-red-600 text-sm">{errors.email.message}</span>}
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="mensagem">Mensagem</label>
+              <textarea 
+                id="mensagem"
+                className="border border-solid border-zinc-200 rounded shadow-sm h-20 px-3 py-2 focus:border-laranja-forte"
+                {...register("mensagem")} 
+              />
+              {errors.mensagem && <span className="text-red-600 text-sm">{errors.mensagem.message}</span>}
+            </div>
+            <div className="flex justify-center items-center mt-10 mb-2">
+              <button 
+                type="submit" 
+                className="bg-verde text-white rounded-xl w-44 h-10 hover:bg-verde-escuro shadow-lg"
+              >
+                Enviar
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-      <nav className="bottom-0 flex justify-center items-center bg-cinza-claro p-4 shadow-xl top-shadow"> 
-        <Link href="/" className="text-2xl sm:text-xl mx-2">
-          <Instagram />
-        </Link>
-        <a href="https://wa.me/5551996739085" className="text-2xl sm:text-xl mx-2">
-          <Whatsapp />
-        </a>
-    </nav>
+      </div>
     </div>
   )
 }
