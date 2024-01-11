e7fe7e2397b5557efb3c4c2810c624af5ff8a30d
Paloma Wink <paloma.wink@hotmail.com>
2024-01-11 17:10:41 -0300

    feat: Criando a funcionalidade do scroll

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,6 +10,21 @@ function Navbar() {
   function toggleMenu() {
     setMenu(!menu)
   }
+
+  const handleScroll = (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+    console.log(id);
+    
+    const element = document.getElementById(id);
+    console.log(element);
+    
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  };
   
   return (
       <>
@@ -21,15 +36,14 @@ function Navbar() {
             <i className="bi bi-list"></i>
           </button>
         </div>
-
         {/* Links do menu */}
         <div className={`${menu ? 'block' : 'hidden'} sm:flex flex-grow justify-evenly items-center h-14 shadow-xl`}>
           <button onClick={toggleMenu} className="absolute top-0 right-0 p-4 sm:hidden">
             <i className="bi bi-x-lg"></i>
           </button>
-          <Link href="/servicos" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Serviços</Link>
-          <Link href="/sobre" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Sobre nós</Link>
-          <Link href="/contato" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Contato</Link>
+          <Link onClick={(e) => handleScroll(e, 'servicos')} href="#servicos" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Serviços</Link>
+          <Link onClick={(e) => handleScroll(e, 'sobre')} href="#sobre" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Sobre nós</Link>
+          <Link onClick={(e) => handleScroll(e, 'contato')} href="#contato" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Contato</Link>
           <a href="https://wa.me/5551996739085" className="sm:text-xl">
             <Whatsapp />
           </a>
