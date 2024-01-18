6c440f78bdd98cae1e85e5662abb85768d59ef9c
Paloma Wink <paloma.wink@hotmail.com>
2024-01-18 13:14:00 -0300

    style: Criando responsividade da pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,11 @@ export default function Home() {
         <div className="absolute inset-0 bg-black opacity-65"></div>
         <div className="relative z-10 flex justify-center items-center h-full">
           <div className="absolute top-0 left-0 p-4 z-20">
-            <Image src={logoDetran} alt='Logo do Detran' width={250} height={250} />
+            <Image src={logoDetran} alt='Logo do Detran' className="w-full sm:w-2/3 md:w-3/4 lg:w-3/4 xl:w-3/4 xs-custom:w-20"/>
           </div>
           <div className="text-center">
-            <h1 className="text-laranja-claro text-7xl mt-4 shadow-lg">DESPACHANTE VISÃO</h1>
-            <p className="text-gray-400 text-2xl mt-4">Sua melhor opção em serviços de despachante</p>
+            <h1 className="text-laranja-claro text-7xl mt-4 shadow-lg sm:text-5xl md:text-6xl lg:text-7xl xl:text-7xl xs-custom:text-3xl">DESPACHANTE VISÃO</h1>
+            <p className="text-gray-400 text-2xl mt-4 sm:text-1xl md:text-2xl lg:text-2xl xl:text-2xl xs-custom:text-lg">Sua melhor opção em serviços de despachante</p>
           </div>
         </div>
       </div>
