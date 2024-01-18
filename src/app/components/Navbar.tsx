779a6a3cf7a5cd152cbb2427fc59327b0159befc
Paloma Wink <paloma.wink@hotmail.com>
2024-01-18 13:56:13 -0300

    style: Tentativa de navbar responsivo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -13,10 +13,8 @@ function Navbar() {
 
   const handleScroll = (e: React.MouseEvent, id: string) => {
     e.preventDefault();
-    console.log(id);
     
     const element = document.getElementById(id);
-    console.log(element);
     
     if (element) {
       window.scrollTo({
@@ -25,7 +23,12 @@ function Navbar() {
       });
     }
   };
-  
+
+  const handleLinkClick = (e: React.MouseEvent, id: string) => {
+    handleScroll(e, id);
+    setMenu(false);
+  }
+
   return (
       <>
       {menu && <div className="overlay" onClick={toggleMenu}></div>}
@@ -37,13 +40,13 @@ function Navbar() {
           </button>
         </div>
         {/* Links do menu */}
-        <div className={`${menu ? 'block' : 'hidden'} sm:flex flex-grow justify-evenly items-center h-14 shadow-xl`}>
+        <div className={`${menu ? 'block' : 'hidden'} sm:flex flex-grow justify-evenly items-center h-14 shadow-xl xs-custom:bg-cinza-claro`}>
           <button onClick={toggleMenu} className="absolute top-0 right-0 p-4 sm:hidden">
             <i className="bi bi-x-lg"></i>
           </button>
-          <Link onClick={(e) => handleScroll(e, 'servicos')} href="#servicos" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Serviços</Link>
-          <Link onClick={(e) => handleScroll(e, 'sobre')} href="#sobre" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Sobre nós</Link>
-          <Link onClick={(e) => handleScroll(e, 'contato')} href="#contato" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Contato</Link>
+          <Link onClick={(e) => handleLinkClick(e, 'servicos')} href="#servicos" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Serviços</Link>
+          <Link onClick={(e) => handleLinkClick(e, 'sobre')} href="#sobre" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Sobre nós</Link>
+          <Link onClick={(e) => handleLinkClick(e, 'contato')} href="#contato" className={`${menu ? 'mobile-menu-link' : 'navbar-button'}`}>Contato</Link>
           <a href="https://wa.me/5551996739085" className="sm:text-xl">
             <Whatsapp />
           </a>
